42a8c44de80d3cddfd8f578f33ff2fc643939efc
Manuel <manuelsapology@gmail.com>
2019-06-28 21:48:00 -0400

    creado el primer reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+	token: null,
+	name: null,
+	email: null,
+	username: null
+};
+
+const rootReducer = (state = initialState, action) => {
+	return state;
+};
+
+export default rootReducer;
