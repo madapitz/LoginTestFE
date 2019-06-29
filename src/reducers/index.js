7247d0d71f100552f9152a695344ceee466befd3
Manuel <manuelsapology@gmail.com>
2019-06-28 22:06:09 -0400

    refactor del store:

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import * as actionTypes from '../actionTypes/index';
+
 const initialState = {
 	token: null,
 	name: null,
@@ -6,6 +8,16 @@ const initialState = {
 };
 
 const rootReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case actionTypes.SET_USER_DATA:
+			state.token = action.payload.token;
+			state.name = action.payload.name;
+			state.email = action.payload.email;
+			state.username = action.payload.username;
+			break;
+		default:
+			return state;
+	}
 	return state;
 };
 
