e795d7525b204e2f070d94e7a5ce060d6f40d3bb
Manuel <manuelsapology@gmail.com>
2019-07-01 20:39:44 -0400

    redux-thunk configurado

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
 
 const routing = (
-	<Router>
-		<Switch>
-			<Route path="/" component={App} />
-		</Switch>
-	</Router>
+	<Provider store={store}>
+		<Router>
+			<Switch>
+				<Route path="/" component={App} />
+			</Switch>
+		</Router>
+	</Provider>
 );
 
 ReactDOM.render(routing, document.getElementById('root'));
